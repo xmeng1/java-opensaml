282fd41d8bee1c411a17fa680947a0ee0e6ffd91
Brent Putman <putmanb@georgetown.edu>
2018-12-12 18:11:27 -0500

    Tweak reloading metadata resolver logging, when new data was invalid.

--- opensaml-saml-impl/src/main/java/org/opensaml/saml/metadata/resolver/impl/AbstractReloadingMetadataResolver.java
+++ opensaml-saml-impl/src/main/java/org/opensaml/saml/metadata/resolver/impl/AbstractReloadingMetadataResolver.java
@@ -506,7 +506,7 @@ public abstract class AbstractReloadingMetadataResolver extends AbstractBatchMet
      */
     protected void processPreExpiredMetadata(final String metadataIdentifier, final DateTime refreshStart,
             final byte[] metadataBytes, final XMLObject metadata) {
-        log.warn("{} Entire metadata document from '{}' was expired at time of loading, existing metadata retained",
+        log.warn("{} Entire metadata document from '{}' was expired at time of loading, previous metadata retained, if any",
                 getLogPrefix(), metadataIdentifier);
 
         nextRefresh = new DateTime(ISOChronology.getInstanceUTC()).plus(getMinRefreshDelay());
